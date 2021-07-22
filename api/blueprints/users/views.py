09b9fa90ca6243ed6564d87affb56f8295011a5c
joyeelam <joyee.l95@gmail.com>
2021-07-22 10:15:26 +0800

    edited views.py

--- api/blueprints/users/views.py
+++ api/blueprints/users/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, make_response
+from flask import Blueprint, request, make_response, jsonify
 from flask_cors import cross_origin
 from flask_jwt_extended import create_access_token
 from werkzeug.security import check_password_hash
@@ -17,7 +17,7 @@ def create():
     user = User(username=username, email=email, password=password)
     if user.save():
         access_token = create_access_token(identity=user.id)
-        return jsonify({'token': access_token, 'msg': 'User successfully created', 'user_id': user.id}), 201
+        return jsonify()
     else:
         return make_response('Request failed', 500)
 
@@ -33,7 +33,7 @@ def create_token():
         result = check_password_hash(hashed_password, password)
         if result:
             access_token = create_access_token(identity=user.id)
-            return jsonify({'token': access_token, 'user_id': user.id}), 201
+            return jsonify({'token': access_token, 'user_id': user.id})
     else:
         return jsonify({'msg': 'Bad username or password'}), 401
 
@@ -42,15 +42,15 @@ def create_token():
 def check_username(username):
     existing_username = User.get_or_none(User.username == username)
     if existing_username:
-        return jsonify({'valid': False}), 400
+        return {'valid': False}
     else:
-        return jsonify({'valid': True}), 200
+        return {'valid': True}
 
 @users_api_blueprint.route('/check_email/<email>', methods=['GET'])
 @cross_origin()
 def check_email(email):
     existing_email = User.get_or_none(User.email == email)
     if existing_email:
-        return jsonify({'valid': False}), 400
+        return {'valid': False}
     else:
-        return jsonify({'valid': True}), 200
+        return {'valid': True}

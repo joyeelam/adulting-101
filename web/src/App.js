ab2324e8ba58f1844ecbb7bc3a3a0625ee6d3b47
joyeelam <joyee.l95@gmail.com>
2021-07-24 12:13:21 +0800

    added save score to db function

--- web/src/App.js
+++ web/src/App.js
@@ -17,6 +17,7 @@ function App() {
   const logOut = (e) => {
     e.preventDefault()
     localStorage.removeItem('token')
+    localStorage.removeItem('id')
     setLoggedIn(!loggedIn)
     history.push('/')
   }

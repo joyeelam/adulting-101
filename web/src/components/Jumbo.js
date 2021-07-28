4b071c0481950d4c684cb59f8faa95c5a4c0d2ec
rachel <rachelxzlim@gmail.com>
2021-07-28 16:40:20 +0800

    update title

--- web/src/components/Jumbo.js
+++ web/src/components/Jumbo.js
@@ -1,4 +1,4 @@
-import {Jumbotron, Container} from 'reactstrap'
+import { Jumbotron, Container } from 'reactstrap'
 import './static/Jumbo.css'
 
 
@@ -7,9 +7,9 @@ const Jumbo = () => {
     <div className="jum">
       <Jumbotron fluid>
         <Container fluid>
-          <h1 className='title'>Welcome to Adulthood 101!</h1>
+          <h1 className='title'>Welcome to Adulting 101!</h1>
           <p className='lead'>A smart, funny, and essential survival guide that you didn't know you needed. But you do. As any current or former 20-something knows, adult life can get a bit weird because no one tells you what to expect. Many of us spend a decade or more figuring out how the world works, hoping that by age 30 our friends are too old to remember what happened. How does one "adult"? Being an adult doesn't come with directions!</p>
-          <br/>
+          <br />
           <p><i className="fas fa-paw"></i></p>
         </Container>
       </Jumbotron>
